0206b735e9283f1f52fd80bd7ef2277454312d62
sfilippenko <fil_mf@mail.ru>
2025-05-09 15:27:30 +0300

    mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule, AuthModule, ReviewModule, TopPageModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    ProductModule,
+    AuthModule,
+    ReviewModule,
+    TopPageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
